a37079069c46243ec07e5ba3f0c78ced34333602
Yashan Bansal <71026956+yashanbansal@users.noreply.github.com>
2021-05-11 13:48:00 +0530

    Update Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -17,72 +17,72 @@ const Data = [
     {
         imgsrc: html,
         title: "HTML",
-        disc: "HTML is a markup language used by the browser to manipulate text, images, and other content, in order to display it in the required format.",
+        disc: "HTML is a markup language used by the browser to manipulate text.",
     },
     {
         imgsrc: css,
         title: "CSS",
-        disc: "CSS is used to control the style of a web document in a simple and easy way. CSS is the acronym for Cascading Style Sheet.",
+        disc: "CSS is used to control the style of a web document in a simple way.",
     },
     {
         imgsrc: react,
         title: "ReactJS",
-        disc:"React is a declarative, efficient, and flexible JavaScript library for building user interfaces It is used for designing user-interface. ",
+        disc:"React is a flexible JavaScript library for building user interfaces. ",
     },
     {
         imgsrc: nodejs,
         title: "NodeJS",
-        disc:"Node.js is a very powerful JavaScript-based platform built on Google Chrome's JavaScript V8 Engine. ",
+        disc:"Node.js is a very powerful JavaScript-based platform. ",
     },
     {
         imgsrc: mongo,
         title: "MongoDB",
-        disc:"MongoDB, the most popular NoSQL database, is an open-source document-oriented database that is designed to store a large scale of data.",
+        disc:"MongoDB, the most popular NoSQL database.",
     },
     {
         imgsrc: python,
         title: "Python Programming",
-        disc:"Python is a widely used general-purpose, high level programming language.Python is designed to be highly readable.",
+        disc:"Python is a widely used general-purpose language.",
     },
     {
         imgsrc: sql,
         title: "SQL (Structured Query Language",
-        disc:"SQL is Structured Query Language, which is a computer language for stored in a relational database. ",
+        disc:"SQL is a computer language for stored in a relational database. ",
     },
     {
         imgsrc: c,
         title: "C Programming",
-        disc:"C is a powerful general-purpose programming language.C language is a robust language with rich set of built-in functions.",
+        disc:"C is a powerful general-purpose programming language.",
     },
     {
         imgsrc: cpp,
         title: "C++ Programming",
-        disc:"The prime purpose of C++ programming was to add object orientation to the C programming language.",
+        disc:"The prime purpose of C++ programming was to add object orientation.",
     },
     {
         imgsrc: ds,
         title: "Data Science",
-        disc:"it is a scientific approach that uses applied statistical and mathematical theory and computer tools to process big data.",
+        disc:"it is a scientific approach that uses applied statistical.",
     },
     {
         imgsrc: dsa,
         title: "Data Structures And Algorithms",
-        disc:"It is a set of algorithms that we can use in any programming language to structure the data in the memory.",
+        disc:"It is a set of algorithms that we can use in any programming.",
     },
     {
         imgsrc: bsp,
         title: "BootStrap",
-        disc:"Bootstrap is the most popular CSS Framework for developing responsive and mobile-first websites.",
+        disc:"Bootstrap is the most popular CSS Framework.",
     },
     {
         imgsrc: js,
         title: "Java Script",
-        disc:"JavaScript is the Programming Language for the Web. JavaScript can update and change both HTML and CSS. ",
+        disc:"JavaScript is the Programming Language for the Web. ",
     },
     {
         imgsrc: java,
         title: "JAVA",
-        disc:"Java is a general-purpose, class-based, object-oriented programming language designed for having lesser implementation dependencies.",
+        disc:"Java is a general-purpose OOPS language dependencies.",
     },
 ];
 
