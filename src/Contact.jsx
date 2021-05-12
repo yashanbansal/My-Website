abdedf1c672ef847dfe2610b0ff4e1fcf3a0a8b4
Yashan Bansal <71026956+yashanbansal@users.noreply.github.com>
2021-05-13 02:37:38 +0530

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,69 +1,42 @@
 
 import React from 'react';
 import { useState } from 'react';
-
+import { db } from "./firebase.jsx";
 
 
   const Contact = () => {
-    const [data ,setData] =useState({
-        fullname:'',
-        phone:'',
-        email:'',
-        msg:'',
-    });
- 
-    const InputEvent =(event) =>{
-        const {name, value} = event.target;
-
-        setData((preVal) =>{
-            return {
-                ...preVal,
-                [name]:value,
-            };
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [phone, setPhone] = useState("");
+    const [loader, setLoader] = useState(false);
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setLoader(true);
+  
+      db.collection("Visiters")
+        .add({
+          name: name,
+          email: email,
+          phone: phone,
+          message: message,
+        })
+        .then(() => {
+          setLoader(false);
+          alert("Your message has been submitted👍");
+        })
+        .catch((error) => {
+          alert(error.message);
+          setLoader(false);
         });
+  
+      setName("");
+      setEmail("");
+      setMessage("");
+      setPhone("");
     };
-    const formSubmit = (e) =>{
-        e.preventDefault();
-        alert(
-            ` Thanku Your Form Submitted Successfully. 
-              Yours details are : 
-              Name -> ${data.fullname}. 
-              Mobile Number -> ${data.phone}. 
-              Email -> ${data.email}. 
-              Your Message -> ${data.msg}`
-        );
-
-    };
-    // const [fullname, setfullname] = useState('');
-    // const [phone, setphone] = useState('');
-    // const [email, setemail] = useState('');
-    // const [msg, setmsg] = useState('');
-    // const InputEvent = (e) =>{
-    //   setfullname(e.target.value);
-    // };
-    // const InputEvent1 = (e) =>{
-    //   setphone(e.target.value);
-    // };
-    // const InputEvent2 = (e) =>{
-    //   setemail(e.target.value);
-    // };
-    // const InputEvent3 = (e) =>{
-    //   setmsg(e.target.value);
-    // };
-    
-      
-      
-
-    // const createToDo = () =>{
-    //   const todoRef =firebase.database().ref('Visiters');
-    //   const todo = {
-    //     fullname,
-    //     phone,
-    //     email,
-    //     msg,
-    //   };
-    //   todoRef.push(todo);
-    // };
+   
 
     return (
     <>
@@ -73,7 +46,7 @@ import { useState } from 'react';
     <div className="container contact-div">
         <div className="row">
             <div className="col-md-6 col-10 mx-auto">
-                <form onSubmit={formSubmit}>
+                <form onSubmit={handleSubmit}>
 
                 
   <div className=" mb-3 ">
@@ -82,8 +55,8 @@ import { useState } from 'react';
     className="form-control" 
     id="name" 
     name="fullname" 
-    value={data.fullname}  
-    onChange={InputEvent} 
+    value={name}  
+    onChange={(e) => setName(e.target.value)} 
     placeholder="Enter Your Full Name" required/>
    
   </div>
@@ -94,8 +67,8 @@ import { useState } from 'react';
     className="form-control" 
     id="mob" 
     name="phone" 
-    value={data.phone} 
-    onChange={InputEvent} 
+    value={phone} 
+    onChange={(e) => setPhone(e.target.value)} 
     placeholder="XXXXXXXXXX" required/>
   </div>
   <div className=" mb-3 ">
@@ -104,21 +77,22 @@ import { useState } from 'react';
     className="form-control" 
     id="exampleFormControlInput1" 
     name="email" 
-    value={data.email} 
-    onChange={InputEvent} 
+    value={email} 
+    onChange={(e) => setEmail(e.target.value)} 
     placeholder="name@example.com" required/>
   </div>
   <div className=" mb-3 ">
     <label for="exampleFormControlTextarea1">Message</label>
     <textarea className="form-control" 
     name="msg" 
-    value={data.msg} 
-    onChange={InputEvent}  
+    value={message} 
+    onChange={(e) => setMessage(e.target.value)}  
     id="exampleFormControlTextarea1" 
     rows="3" required></textarea>
   </div>
   <div className="col-10">
   <button 
+  style={{ background: loader ? "#ccc" : " rgb(2, 2, 110)"}}
   className="btn btn-outline-primary mb-2">Submit Form</button>
   </div>
 </form>
