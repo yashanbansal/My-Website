e9cf18200d14e3e7db9317cc653d00951af33a2a
Yashan Bansal <71026956+yashanbansal@users.noreply.github.com>
2021-05-30 16:30:43 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -16,9 +16,9 @@ const Home = () => {
                         <strong className="brand-name"> Chitkara University</strong>
                     </h3>
                     <h5 className="my-3">
-                    A Fervent person and proficient in Python 
+                    A Fervent person and proficient in Java 
                     and Full Stack Web Development seeks to establish 
-                    a career in the field of Web Development and Data Science 
+                    a career in the field of Web Development 
                     in a progressive organization where I can enhance my skills 
                     and learning to contribute to the success of the organization.
                     </h5>
